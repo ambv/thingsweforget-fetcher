f9b817e5e2064bd7c766d27d77d83d8401f32d8e
Łukasz Langa <lukasz@langa.pl>
2016-04-27 03:52:44 -0700

    0.1.1

--- src/langacore/thingsweforget/__init__.py
+++ src/langacore/thingsweforget/__init__.py
@@ -7,6 +7,6 @@ This project requires Python 3.1 or later.
 
 # UTF-8 in the variables below is PyPI incompatible
 __version__ = "0.1"
-__release__ = "0.1.0"
+__release__ = "0.1.1"
 __author__ = "Lukasz Langa"
 __contact__ = "lukasz@langa.pl"
